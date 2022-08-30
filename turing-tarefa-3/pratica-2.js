d7a16d40046db8f02e541ba15e5ce9c4623ea2e3
Isadora Nogueira <misadorasousa@hotmail.com>
2022-08-30 19:20:16 -0300

    adicionou-cod-pratica-2.3

---
+++ turing-tarefa-3/pratica-2.js
@@ -0,0 +1,10 @@
+let multiplicador = 7
+let n = 10
+let i = 1
+
+while (i <= n) {
+    let resultado = multiplicador * i
+    let valorExibido = `${multiplicador} X ${i} = ${resultado}`
+    console.log(valorExibido)
+    i++
+}
