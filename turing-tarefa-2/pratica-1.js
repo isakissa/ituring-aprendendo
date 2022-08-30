8974cc4bde511406bccf32e695628b0c68a4ddee
Isadora Nogueira <misadorasousa@hotmail.com>
2022-08-30 02:16:31 -0300

    adicionou-cod-pratica-1

--- turing-tarefa-2/pratica-1.js
+++ turing-tarefa-2/pratica-1.js
@@ -4,8 +4,7 @@ let profissao = "estudante"
 let idade = 21
 let viagemDosSonhos = "Tailandia"
 
-console.log(nome)
-console.log(email)
-console.log(profissao)
-console.log(idade)
-console.log(viagemDosSonhos)
+let frase = "Olá, "+nome+"! Somos aqui da agência de turismo Viagem dos Sonhos. Estamos te escrevendo este email, pois acabamos de confirmar as compras das passagens aéreas para "+viagemDosSonhos+". Confirma pra gente alguns dados? A sua idade é "+idade+", você é "+profissao+" e o seu email é "+email+" ? Ficamos no aguardo. Muito obrigado e boa viagem! :D"
+
+
+console.log(frase)
