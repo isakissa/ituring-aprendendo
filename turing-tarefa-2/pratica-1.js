dac891d47a2f9030969842656e9bc7279493507a
Isadora Nogueira <misadorasousa@hotmail.com>
2022-08-30 01:54:30 -0300

    adicionou-cod-pratica-1

--- turing-tarefa-2/pratica-1.js
+++ turing-tarefa-2/pratica-1.js
@@ -3,3 +3,9 @@ let email = "misadorasousa@hotmail.com"
 let profissao = "estudante"
 let idade = 21
 let viagemDosSonhos = "Tailandia"
+
+console.log(nome)
+console.log(email)
+console.log(profissao)
+console.log(idade)
+console.log(viagemDosSonhos)
